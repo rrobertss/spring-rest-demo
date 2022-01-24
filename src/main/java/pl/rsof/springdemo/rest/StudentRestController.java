62a188fbd069fc5fcba19c07bbc91e60e486a16d
Robert Siwak <rsiwak@gmail.com>
2022-01-24 14:48:10 +0100

    spring-rest-demo: student by id

--- src/main/java/pl/rsof/springdemo/rest/StudentRestController.java
+++ src/main/java/pl/rsof/springdemo/rest/StudentRestController.java
@@ -9,7 +9,10 @@ package pl.rsof.springdemo.rest;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,18 +26,35 @@ import pl.rsof.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-
+	
+	
+	private List<Student>students;
+	
+	// load only once
+	@PostConstruct
+	public void loadData() {
+		students = new LinkedList<>();
+		Student s1 = new Student("Maria", "Nowak"); students.add(s1);
+		Student s2 = new Student("Irena", "Bąk"); students.add(s2);
+		Student s3 = new Student("Ireneusz", "Kowalski"); students.add(s3);
+	}
+	
+	
+	
 
 	// define endpoint for "/students" - return list of students
 	@GetMapping("/students")
 	public List<Student> getStudentsList() {
-		
-		List<Student>list = new LinkedList<>();
-		Student s1 = new Student("Maria", "Nowak"); list.add(s1);
-		Student s2 = new Student("Irena", "Bąk"); list.add(s2);
-		Student s3 = new Student("Ireneusz", "Kowalski"); list.add(s3);
 	
-		return list;
+		return students;
 	}
+	
+	
+	// endpoint for /student/{studentId} - return student at index
+	@GetMapping("/students/{studentId}")
+	public Student getStudentById(@PathVariable int studentId) {
+		return students.get(studentId);
+	}
+	
 	
 }
